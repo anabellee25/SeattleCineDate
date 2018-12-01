3ebc85b28e45dd163e4f6c428ad33b5361aa0322
Ana Lee <analee@Anas-MacBook-Pro.local>
2018-11-30 21:02:16 -0800

    fixed suggestions

--- js/suggestions.js
+++ js/suggestions.js
@@ -8,45 +8,48 @@ $(document).ready(function () {
         messagingSenderId: "623547631250"
     };
     firebase.initializeApp(config);
-
+ 
     let database = firebase.database();
-
+ 
     let user = "";
     let comment = "";
-
-
+ 
+ 
     $("#sug-button").on("click", function (event) {
         event.preventDefault();
-
+ 
         user = $("#formGroupExampleInput").val().trim();
         comment = $("#formGroupExampleInput2").val();
-
+ 
         console.log(user, comment);
-       
-
+ 
+        if (user === "" || comment === ""){
+            $("#formGroupExampleInput2").attr("placeholder", "Whoops! Looks like you didn't add a comment. Please add a suggestion!");
+    }else {
+ 
         database.ref().push({
             user: user,
             comment: comment
         });
-
-
-
-
-
-    });
-
-    database.ref().on("child_added", function(snapshot){
-
-        let newRow = $("<tr></tr>");
-        let newUser = $("<td></td>").text(snapshot.val().user);
-        let newComment = $("<td></td>").text(snapshot.val().comment);
-
-        newRow.append(newUser, newComment);
-        $("#firebase-data").append(newRow);
-
-        
-
-
-    });
-
-});
+ 
+    }
+ 
+ 
+ 
+ });
+ 
+ database.ref().on("child_added", function (snapshot) {
+ 
+    let newRow = $("<tr></tr>");
+    let newUser = $("<td></td>").text(snapshot.val().user);
+    let newComment = $("<td></td>").text(snapshot.val().comment);
+ 
+    newRow.append(newUser, newComment);
+    $("#firebase-data").append(newRow);
+ 
+ 
+ 
+ 
+ });
+ 
+ });
